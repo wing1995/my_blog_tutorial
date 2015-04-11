c0fc58ae1d90f26684d4e39e8121184954027846
wing1995 <1130384613@qq.com>
2015-04-11 08:25:33 -0700

    rss and paginator

--- article/models.py
+++ article/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.urlresolvers import reverse
 
 # Create your models here.
 class Article(models.Model):
@@ -6,8 +7,12 @@ class Article(models.Model):
     category = models.CharField(max_length=50, blank=True)
     date_time = models.DateField(auto_now_add=True)
     content = models.TextField(blank=True, null=True)
+    
+    def get_absolute_url(self):
+        path = reverse('detail', kwargs={'id': self.id})
+        return "http://127.0.0.1:8000%s" % path
 
-    def __unicode__(self):
+    def __str__(self):
         return self.title
 
     class Meta:
